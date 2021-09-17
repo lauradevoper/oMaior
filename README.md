ffefeaf8670e5e094c1d120fcd9cd0937c746dc6
Laura Jones <developerlaurajones@hotmail.com>
2021-09-17 20:19:11 -0300

    Resolução desafio O Maior

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# oMaior
+# O Maior
+
+## Desafio
+Desenvolva um programa capaz de ler três valores e apresentar o maior deles e adicionar a mensagem “ eh o maior”. Use a seguinte forma como base:
---
+++ resolução.cs
@@ -0,0 +1,28 @@
+class Program
+{
+    static void Main(string[] args)
+    {
+        string[] vet = Console.ReadLine().Split(' ');
+        
+        int a = Convert.ToInt32(vet[0]);
+        int b = Convert.ToInt32(vet[1]);
+        int c = Convert.ToInt32(vet[2]);
+        int MAIOR = 0;
+
+        if(a > b && a > c)
+        {
+            MAIOR = a;
+        }
+        else if(b > a && b > c)
+        {
+            MAIOR = b;
+        }
+        else
+        {
+            MAIOR = c;
+        }
+
+        Console.WriteLine($"{MAIOR} eh o maior");
+
+    }
+} 
